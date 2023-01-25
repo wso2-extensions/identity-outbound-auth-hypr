6a2e45db33d932bcc8a18a3d26e97b715eef8af0
Afra Hussaindeen <47135592+AfraHussaindeen@users.noreply.github.com>
2023-01-25 11:04:48 +0530

    Update components/org.wso2.carbon.identity.application.authenticator.hypr.common/src/main/java/org/wso2/carbon/identity/application/authenticator/hypr/common/constants/HyprAuthenticatorConstants.java
    Co-authored-by: Wenod Pathirana <wenod.pathirana@gmail.com>

--- components/org.wso2.carbon.identity.application.authenticator.hypr.common/src/main/java/org/wso2/carbon/identity/application/authenticator/hypr/common/constants/HyprAuthenticatorConstants.java
+++ components/org.wso2.carbon.identity.application.authenticator.hypr.common/src/main/java/org/wso2/carbon/identity/application/authenticator/hypr/common/constants/HyprAuthenticatorConstants.java
@@ -15,7 +15,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.wso2.carbon.identity.application.authenticator.hypr.common.constants;
 
 import java.util.Arrays;
